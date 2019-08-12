8f661d72f82e3f9db98d821b597e9f80273b7e21
Mickael Coelho <mick.coelho@gmail.com>
2019-08-12 13:20:22 -0700

    Add app component

---
+++ src/pages/_app.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import { func, number } from 'prop-types';
+import ReactGA from 'react-ga';
+import Router from 'next/router';
+
+import withRematch from '../store/utils/withRematch';
+import { store } from '../store';
+
+import styles from '../style/index.css'; // eslint-disable-line
+
+class PApp extends App {
+  static async getInitialProps({ Component, ctx, query }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    const { pathname } = ctx;
+    pageProps.pathname = pathname;
+    return { pageProps, query };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
+
+const mapState = state => ({
+  scrollSpeed: state.app.scrollSpeed,
+});
+
+const mapDispatch = ({ app: { setProjectSlug, setIsTouch, setScrollSpeed, setPageScrollTop, setAboutPanelOpen } }) => ({
+  setProjectSlug,
+  setIsTouch,
+  setScrollSpeed,
+  setPageScrollTop,
+  setAboutPanelOpen,
+});
+
+export default withRematch(store, mapState, mapDispatch)(PApp);
