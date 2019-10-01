e918f53452557e9c68ec8ef8a7cb12db7b9bd830
Mick Coelho <mick.coelho@gmail.com>
2019-10-01 16:26:10 -0700

    more tests

--- functions/hello-name-post.js
+++ functions/hello-name-post.js
@@ -1,6 +1,11 @@
 const querystring = require('querystring');
 
 exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: event.body,
+  };
+
   // Only allow POST
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
@@ -8,7 +13,6 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
-  const params = querystring.parse(event.body);
   const name = params.name || 'World';
 
   return {
