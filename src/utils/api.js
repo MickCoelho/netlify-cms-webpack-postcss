535c1c19b67916a9e38100011aa2dfdd16e886d0
Mick Coelho <mick.coelho@gmail.com>
2019-10-02 16:18:39 -0700

    remove stupid thing

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,21 +1,18 @@
 import fetch from 'node-fetch';
 
 const readAll = () => {
-  return fetch(
-    'http://localhost:9000/.netlify/functions/customers-read-all',
-  ).then((response) => {
-    return response.json();
-  });
+  return fetch('/.netlify/functions/customers-read-all').then(
+    (response) => {
+      return response.json();
+    },
+  );
 };
 
 const create = (data) => {
-  return fetch(
-    'http://localhost:9000/.netlify/functions/todos-create',
-    {
-      body: JSON.stringify(data),
-      method: 'POST',
-    },
-  ).then((response) => {
+  return fetch('/.netlify/functions/todos-create', {
+    body: JSON.stringify(data),
+    method: 'POST',
+  }).then((response) => {
     return response.json();
   });
 };
